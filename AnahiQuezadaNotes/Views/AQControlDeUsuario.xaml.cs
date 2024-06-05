a5eb190b5561465da6e8df45962b60af8bf3bf78
Anahi Quezada <anahi.quezada@udla.edu.ec>
2024-06-05 11:17:33 -0500

    Se modifico el TabBar, ahora se muestra Conteo en la parte de abajo con su correcta funcionalidad

--- AnahiQuezadaNotes/Views/AQControlDeUsuario.xaml.cs
+++ AnahiQuezadaNotes/Views/AQControlDeUsuario.xaml.cs
@@ -9,6 +9,38 @@ public partial class AQControlDeUsuario : ContentPage
 
     private void btnCalcular_Clicked(object sender, EventArgs e)
     {
+        string input = inputEntry.Text;
+        int conteoDigito = 0;   
+        int conteoLetra = 0;
+        int conteoVocal = 0;
+        int conteoMinuscula = 0;
+        int conteoMayuscula = 0;
+        int conteoTotal = input.Length;
+
+        foreach (char c in input)
+        {
+            if(char .IsDigit(c))
+            {
+                conteoDigito++;
+            }
+            else if (char .IsDigit(c))
+            {
+                conteoLetra++;
+                if (c.ToString().ToLower() == c.ToString())
+                {
+                    conteoMinuscula++;
+                }
+                else 
+                { 
+                    conteoMayuscula++;
+                }
+            }
+        }
+
+    }
+
+    private void btnLimpiar_Clicked(object sender, EventArgs e)
+    {
 
     }
 }
